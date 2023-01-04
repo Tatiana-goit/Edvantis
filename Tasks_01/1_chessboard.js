7211d077fe3410f7719c7efcfb57785c13af6cbc
Tatsiana  Taranushchenko <tatsiana.taranushchenko@trustradius.com>
2023-01-04 16:11:13 +0300

    edit task

--- Tasks_01/1_chessboard.js
+++ Tasks_01/1_chessboard.js
@@ -9,17 +9,68 @@
 // _#_#_#_#
 // #_#_#_#_
 
-// -------------------------------------- VERSION 1: Q = n = 8 -?
-
-function chessBoard(numberOfChars,numberOfRows) {
-  return Array.from(Array(numberOfRows).keys()).reduce((acc, i) => {
-    let result = '';
-    i % 2 === 0 ? result += '_#'.repeat(numberOfChars/2) : result += '#_'.repeat(numberOfChars/2);
-    return acc + result + '\n';
-  }, '')
+function createRows(rowLength) {
+  let rows = {
+      oddRow: '',
+      evenRow: '',
+  }
+  for(let i = 0; i < rowLength; i++) {
+      if(i % 2 == 0) {
+          rows.oddRow += "_";
+          rows.evenRow += "#";
+      } else {
+          rows.oddRow += "#";
+          rows.evenRow += "_";
+      }
+  }
+  return rows;
 }
 
-console.log(chessBoard(8,8));
+function generateBoard(rowQuantity, rows) {
+  let board = '';
+  for(let i = 0; i < rowQuantity; i++) {
+      if (i % 2 == 0) {
+          board += rows.evenRow + "\n\r";
+      } else {
+          board += rows.oddRow + "\n\r";;
+      }
+  }
+  return board;
+}
+
+function render(board) {
+  console.log(board);
+}
+
+function renderChessBoard(rowLength,numberOfRows) {
+  try {
+      rowLength = Number(rowLength);
+      numberOfRows = Number(numberOfRows);
+      // if(!!rowLength || !!numberOfRows ) {
+      //     return "";
+      // }   
+      const rows = createRows(rowLength);
+      const board = generateBoard(numberOfRows, rows);
+      render(board);
+  } catch(e) {
+      console.error(e);
+  }
+}
+
+renderChessBoard(8,8);
+
+
+// -------------------------------------- VERSION 1: Q = n = 8 -?
+
+// function chessBoard(numberOfChars,numberOfRows) {
+//   return Array.from(Array(numberOfRows).keys()).reduce((acc, i) => {
+//     let result = '';
+//     i % 2 === 0 ? result += '_#'.repeat(numberOfChars/2) : result += '#_'.repeat(numberOfChars/2);
+//     return acc + result + '\n';
+//   }, '')
+// }
+
+// console.log(chessBoard(7,7));
 
 // -------------------------------------- VERSION 2: Q = n^2 = 8*8=64
 
